f2a1c01df77f74dfa88f31240e0c5e1150986e58
dreum <dirk.reum@gmail.com>
2014-08-27 02:26:08 +0000

    first test passing

--- bowlingKata.rb
+++ bowlingKata.rb
@@ -5,5 +5,12 @@ class BowlingTest < Test::Unit::TestCase
     def setup
         @game = Game.new
     end
+    
+    def test_gutter_game
+        20.times do
+            @game.roll(0)
+        end
+        assert_equal(0, @game.score())
+    end
 end
 
