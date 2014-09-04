5f9f586d18c5329b7b70eb10716188c5714f0d9c
dreum <dirk.reum@gmail.com>
2014-09-04 01:22:32 +0000

    Roll spare passing

--- Game.rb
+++ Game.rb
@@ -1,12 +1,33 @@
 class Game
     def initialize()
-        @current_score = 0
+        @rolls = []
     end
     
     def roll(pins)
-        @current_score += pins
+        @rolls << pins
     end
+    
+    def spare?(current_roll)
+        frame_score(current_roll) == 10
+    end
+    
+    def frame_score(current_roll)
+        @rolls[current_roll] + @rolls[current_roll + 1]
+    end
+    
     def score()
-        @current_score
+        current_score = 0
+        current_frame = 0
+        current_roll = 0
+        while current_frame < 10
+            if (spare?(current_roll))
+                current_score += frame_score(current_roll) + @rolls[current_roll + 2]
+            else
+                current_score += frame_score(current_roll)
+            end
+            current_roll += 2
+            current_frame += 1
+        end
+        current_score
     end
 end
--- bowlingKata.rb
+++ bowlingKata.rb
@@ -6,20 +6,28 @@ class BowlingTest < Test::Unit::TestCase
         @game = Game.new
     end
     
-    def roll_many(pins)
-        20.times do
+    def roll_many(number, pins)
+        number.times do
             @game.roll(pins)
         end
     end
     
     def test_gutter_game
-        roll_many(0)
+        roll_many(20, 0)
         assert_equal(0, @game.score())
     end
     
     def test_all_ones
-        roll_many(1)
+        roll_many(20, 1)
         assert_equal(20, @game.score())
+    end
+    
+    def test_spares
+        @game.roll(5)
+        @game.roll(5)
+        @game.roll(3)
+        roll_many(17, 0)
+        assert_equal(16, @game.score())
     end
 end
 
